4a0327114beeee2a58aabee76209d18e9a7601b3
abdrahman02 <mabdulr44@gmail.com>
2024-05-27 15:15:05 +0700

    online indicator, realtime chat

--- index.js
+++ index.js
@@ -4,8 +4,35 @@ const io = new Server({
   cors: "http://localhost:5173",
 });
 
+let onlineUser = [];
+
 io.on("connection", (socket) => {
-  // ...
+  console.log("New Connection", socket.id);
+
+  //   Listen to a connection
+  socket.on("addNewUser", (userId) => {
+    !onlineUser.some((user) => user.userId === userId) &&
+      onlineUser.push({ userId, socketId: socket.id });
+
+    console.log("onlineUser", onlineUser);
+
+    io.emit("getOnlineUser", onlineUser);
+  });
+
+  // add message
+  socket.on("sendMessage", (message) => {
+    const user = onlineUser.find((user) => user.userId === message.recipientId);
+    if (user) {
+      io.to(user.socketId).emit("getMessage", message);
+    }
+  });
+
+  // user disconnect
+  socket.on("disconnect", () => {
+    onlineUser = onlineUser.filter((user) => user.socketId !== socket.id);
+
+    io.emit("getOnlineUser", onlineUser);
+  });
 });
 
 io.listen(3000);
